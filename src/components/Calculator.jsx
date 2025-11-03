5cd5c4ec57d5673073e8bc2116412447ff7f6d1e
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-03 17:37:20 +0400

    Add Calculator.jsx

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,12 @@
+// src/components/Calculator.jsx
+import React, { useState } from "react";
+import { add, subtract, multiply, divide } from "../utils/calculator";
+
+export default function Calculator() {
+  // TODO: define state and event handlers
+  return (
+    <div className="calculator">
+      {/* Display and buttons go here */}
+    </div>
+  );
+}
